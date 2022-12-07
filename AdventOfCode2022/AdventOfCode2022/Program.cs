ae27f6c214b6ac98a6ccce4a3461a4a53453aada
Marius Klint Hansen <mariusklint@gmail.com>
2022-12-07 22:46:00 +0100

    Day 1 and 2 completed
    Completed the challenges for day 1 and 2, with first edition.
    Planning to make it more readable.

---
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+List<int> elves = new();
+using (StreamReader reader = new("input.txt"))
+{
+    int calories = 0;
+    while (!reader.EndOfStream)
+    {
+        string? line = reader.ReadLine();
+        if (string.IsNullOrEmpty(line))
+        {
+            elves.Add(calories);
+            calories = 0;
+        }
+        else
+            calories += int.Parse(line);
+    }
+}
+
+StringBuilder stringBuilder = new();
+stringBuilder.AppendLine("The top three elves are carrying:");
+elves = elves.OrderByDescending(x => x).Take(3).ToList();
+for (int i = 0; i < elves.Count; i++)
+{
+    stringBuilder.AppendLine($" {i + 1}: {elves[i]}");
+}
+stringBuilder.AppendLine($"Totalling {elves.Sum()} calories");
+Console.WriteLine(stringBuilder.ToString());
---
+++ AdventOfCode2022/Day2/Program.cs
@@ -0,0 +1,69 @@
+﻿void PartOne()
+{
+
+    Dictionary<char, int> points = new()
+{
+    {'A', 1 },
+    {'B', 2 },
+    {'C', 3 },
+    {'X', 1 },
+    {'Y', 2 },
+    {'Z', 3 },
+};
+
+    int totalPoints = 0;
+    using StreamReader streamReader = new("input.txt");
+    while (streamReader.EndOfStream is not true)
+    {
+        string? line = streamReader.ReadLine();
+        char[] choices = line.Replace(" ", "").ToCharArray();
+        switch (points[choices[1]] - points[choices[0]])
+        {
+            case 1:
+            case -2:
+                totalPoints += points[choices[1]] + 6;
+                break;
+
+            case 0:
+                totalPoints += points[choices[1]] + 3;
+                break;
+
+            default:
+                totalPoints += points[choices[1]];
+                break;
+        }
+    }
+    Console.WriteLine($"Total points: {totalPoints}");
+}
+
+void PartTwo()
+{
+
+    Dictionary<char, int> points = new()
+{
+    {'A', 1 },
+    {'B', 2 },
+    {'C', 3 },
+    {'X', 1 },
+    {'Y', 2 },
+    {'Z', 3 },
+};
+
+    int totalPoints = 0;
+    using StreamReader streamReader = new("input.txt");
+    while (streamReader.EndOfStream is not true)
+    {
+        string? line = streamReader.ReadLine();
+        char[] choices = line.Replace(" ", "").ToCharArray();
+
+        totalPoints += choices[1] switch
+        {
+            'X' => points.FirstOrDefault(x => new int[] { -1, 2 }.Contains(x.Value - points[choices[0]])).Value,
+            'Y' => points.FirstOrDefault(x => new int[] { 0 }.Contains(x.Value - points[choices[0]])).Value + 3,
+            'Z' => points.FirstOrDefault(x => new int[] { 1, -2 }.Contains(x.Value - points[choices[0]])).Value + 6,
+        };
+    }
+
+    Console.WriteLine($"Total points: {totalPoints}");
+}
+PartTwo();
